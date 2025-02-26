4043546e00c92afb88d5454baa5da6e977e48b24
Vasif <sultanovasif@gmail.com>
2025-02-26 13:57:13 +0500

    e

---
+++ WorkoutApp/WorkoutApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkoutApp
+//
+//  Created by Toporusan on 26.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WorkoutApp/WorkoutAppTests/WorkoutAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutAppTests.swift
+//  WorkoutAppTests
+//
+//  Created by Toporusan on 26.02.2025.
+//
+
+import Testing
+@testable import WorkoutApp
+
+struct WorkoutAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
